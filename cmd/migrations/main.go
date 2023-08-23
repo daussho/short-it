e4feb94868172270a86a1f4b7033daf723c5c743
Abdul Hafizh Firdaus <abdul.firdaus@astronauts.id>
2023-08-23 08:26:06 +0700

    fix: migration

--- cmd/migrations/main.go
+++ cmd/migrations/main.go
@@ -1,17 +1,51 @@
 package main
 
 import (
+	"fmt"
+	"log"
+
 	"github.com/daussho/short-it/configs"
 	"github.com/daussho/short-it/models"
+	"golang.org/x/crypto/bcrypt"
+	"gorm.io/gorm"
 )
+
+var db *gorm.DB
 
 func main() {
 	err := configs.InitDB()
 	if err != nil {
 		panic(err)
 	}
-	db := configs.ConnectDB()
 
+	db = configs.ConnectDB()
+
+	log.Print("Input migration number: ")
+	var migrationNumber string
+	_, err = fmt.Scanln(&migrationNumber)
+	if err != nil {
+		panic(err)
+	}
+
+	switch migrationNumber {
+	case "0001":
+		migration0001()
+	case "0002":
+		migration0002()
+	default:
+		log.Fatal("Migration number not found")
+	}
+}
+
+func migration0001() {
 	db.AutoMigrate(&models.User{})
 	db.AutoMigrate(&models.Url{})
+}
+
+func migration0002() {
+	bcryptPassword, _ := bcrypt.GenerateFromPassword([]byte("admin"), bcrypt.DefaultCost)
+	db.Create(&models.User{
+		Username: "admin",
+		Password: string(bcryptPassword),
+	})
 }
--- makefile
+++ makefile
@@ -6,3 +6,6 @@ run-http:
 run-http-nodemon:
 	@echo "Running HTTP server with nodemon"
 	@nodemon -e go,html --signal SIGTERM --exec 'make run-http || exit 1' 
+run-migration:
+	@echo "Running migration"
+	@go run cmd/migrations/main.go
